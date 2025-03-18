99eac5d7bba703f74439f4d64cebc4f27b3a5664
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2025-03-18 21:14:26 +0800

    LOGIN WITHOUT TOKEN

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,3 +74,24 @@ export const updateUser = async (req,res) => {
     return res.status(500).json({message:"Internal Server error!"});    
     }
 };
+
+export const login = async (req,res) => {
+    try {
+    const {email,password} = req.body;
+    if(!email ||!password) {
+        return res.status(400).json({message:"All fields required!"});
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(400).json({message:"Username or password is incorrect!"});
+    }
+    const isPasswordMatch = await bcrypt.compare(password,user.password);
+    if(!isPasswordMatch){
+        return res.status(400).json({message:"Username or password is incorrect!"});
+    }
+    res.status(200).json({message:"Login successfully!",user});      
+    } catch (error) {
+    console.log(`Error in login : ${error.message}`);
+    return res.status(500).json({message:"Internal Server error!"});           
+    }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getAllUsers, registerAccount, updateUser } from '../controllers/authController.js';
+import { getAllUsers, login, registerAccount, updateUser } from '../controllers/authController.js';
 
 const authRoute = express.Router();
 
 authRoute.post("/register-account",registerAccount);
 authRoute.get("/get-all-users",getAllUsers);
 authRoute.put("/update-user/:id",updateUser);
+authRoute.post("/login",login);
 
 export default authRoute;
