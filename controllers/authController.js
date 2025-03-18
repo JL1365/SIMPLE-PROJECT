213a0c25eadfee41cd63518cc51cbb3841b0fcf1
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2025-03-18 20:32:48 +0800

    GET ALL USERS AND EXCLUDE PASSWORD

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,22 @@ export const registerAccount = async (req,res) => {
         res.status(201).json({message:"User registered successfully!",data:newUser});
 
     } catch (error) {
-        console.log(`Error in registering user : ${error.message}`);
-        return res.status(500).json({message:"Internal Server error!"});
+    console.log(`Error in registering user : ${error.message}`);
+    return res.status(500).json({message:"Internal Server error!"});
+    }
+};
+
+export const getAllUsers = async (req,res) => {
+    try {
+    const allUsers = await User.find({},"-password"); //Exclude the password
+    if(allUsers.length === 0){
+        return res.status(400).json({message:"No users found!"});
+    }      
+    
+    res.status(200).json({message:"Users retrieved successfully!",data:allUsers});
+    
+    } catch (error) {
+    console.log(`Error in retrieving user : ${error.message}`);
+    return res.status(500).json({message:"Internal Server error!"});
     }
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { registerAccount } from '../controllers/authController.js';
+import { getAllUsers, registerAccount } from '../controllers/authController.js';
 
 const authRoute = express.Router();
 
 authRoute.post("/register-account",registerAccount);
+authRoute.get("/get-all-users",getAllUsers);
 
 export default authRoute;
